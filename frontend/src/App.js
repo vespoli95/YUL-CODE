51423a94ef1a597c910548371dc803230b9d9dcb
nalanart <alantran96@gmail.com>
2021-01-23 15:49:18 -0500

    commit
    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Main from './components/Main'
+
+function App() {
+  
+  return (
+    <div>
+      <div class="content" >
+        <nav class="navbar navbar-expand-lg navbar-dark color-menu static-top">
+          <div class="container container-global">
+            <a class="navbar-brand" href="#">
+              <img src="../assets/images/YUL_CODE_White.png" alt="" style={{width: "150px"}} />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active menu">
+                  <a class="nav-link menuWidth" href="#">Accueil
+                  </a>
+                </li>
+                <li class="nav-item menu">
+                  <a class="nav-link menuWidth" href="#">Map Montreal</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
